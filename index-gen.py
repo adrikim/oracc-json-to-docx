899e0392cf911e5d8131c3d859ab1b497e0e8863
Adrian Kim <adrikim@umich.edu>
2018-12-02 20:41:31 -0500

    revert

--- index-gen.py
+++ index-gen.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 import json
 import os
-import sys
 import argparse
+
+from docx import Document
 
 """Tool to generate a flat file with metadata of files for use with autokey.
 Each entry contains:
 - Q, X, or P-number (textid; to be used as English title)
 - absolute path to docx file
-- size (or # of lines) of file // obsolete if we can do image matches!
-- museum/common name (to be used as an alias)
+- size (or # of lines) of file
+- museum/popular/primary name (to be used as an alias)
 - other exemplar/museum info (to be used in description)
+
+NOTE: will not render unicode on output, but seems to paste just fine...
 """
 
 # These contain your corpusjson folders as well as catalogue.json
@@ -18,6 +21,7 @@ Each entry contains:
 folders = [
     'ribo/babylon6',
     'ribo/babylon7',
+    'ribo/babylon8',
     'rinap/rinap1',
     'rinap/rinap3',
     'rinap/rinap4',
@@ -29,43 +33,22 @@ folders = [
     'saao/saa17',
     'saao/saa18',
     'saao/saa19',
+    'suhu'
 ]
 
 def _read_catalogue(json_path):
     with open(json_path) as fd:
         return json.load(fd)
 
+def _save_catalogue(my_catalogue, json_path):
+    with open(json_path, 'w+') as outfile:
+        json.dump(my_catalogue, outfile, sort_keys=True, indent=4)
+
 def create_flat_files(oracc_path, docx_parent_path):
     """Output some JSONs... we'll see how we want to format them later
-    museum/common_title museum/exemplar_info
-
-    Relevant info from each project:
-    RINAP:
-    display_name, eg. RINAP 4 001
-    primary_publication/designation, eg. Esarhaddon 001 *
-    collection, eg. British Museum, London, UK; Oriental Institute...
-    exemplars, eg. BM 121005 (0001); K 0123 + K 0234; ...
-    popular_name, eg. K 13733
-
-    RIBO:
-    display_name, eg. RINAP 4 104
-    primary_publication/designation, eg. Esarhaddon 104
-    exemplars, eg. BM 09876 (Bu 18888-05-02), ...
-    collection, eg. British Museum, London, UK; ...
-    popular_name, eg. Babylon A
-
-    SAAO:
-    museum_no, eg. IM 34567
-    primary_publication, eg. NL 986; ND 2345; ...
-    designation/display_name, eg. SAA 01 001
-
-    SUHU:
-    primary_publication/designation, eg. Ninurta-kudurri-usur 01
-    collection, eg. Iraq Museum, Baghdad, Iraq
-    museum_no, eg. possibly IM 090909; IM 123458; IM -
-    popular_name, eg. RIMB 2 S.0.1002.1
     """
     for folder in folders:
+        print(folder)
         my_catalogue = {}
 
         # Parse catalogue JSON
@@ -77,22 +60,85 @@ def create_flat_files(oracc_path, docx_parent_path):
         for textid in members:
             # Check if it's got a docx equivalent
             # If not, don't bother adding it to my catalogue
+            print(textid)
             docx_name = textid + ".docx"
             docx_path = os.path.join(docx_parent_path, folder, docx_name)
             if not os.path.exists(docx_path):
                 continue
 
-            my_catalogue.update({
-                textid: {
-                    "docx_path": docx_path,
-                    "ochre_title": textid,
-                    "abbreviation": None,
-                    "alias": None,
-                    "museum_info": None
-                },
-            })
+            # Count # of lines present
+            document = Document(docx_path)
+            n_lines = len(document.paragraphs)
+
+            text_info = members[textid]
+
+            if "rinap" in folder:
+                my_catalogue.update({
+                    textid: {
+                        "docx_path": docx_path,
+                        "docx_lines": n_lines,
+                        "ochre_title": textid,
+                        "alias": text_info["popular_name"],
+                    },
+                })
+                if "collection" in text_info or "exemplars" in text_info:
+                    my_catalogue[textid].update({
+                        "description": "Collection:\n{0}\nExemplars:\n{1}".format(
+                            text_info.get("collection", ""),
+                            text_info.get("exemplars", "")
+                        ),
+                    })
+
+            elif "ribo" in folder:
+                my_catalogue.update({
+                    textid: {
+                        "docx_path": docx_path,
+                        "docx_lines": n_lines,
+                        "ochre_title": textid,
+                        "alias": text_info["popular_name"],
+                    },
+                })
+                if "collection" in text_info or "exemplars" in text_info:
+                    my_catalogue[textid].update({
+                        "description": "Collection:\n{0}\nExemplars:\n{1}".format(
+                            text_info.get("collection", ""),
+                            text_info.get("exemplars", "")
+                        ),
+                    })
+
+            elif "saao" in folder:
+                my_catalogue.update({
+                    textid: {
+                        "docx_path": docx_path,
+                        "docx_lines": n_lines,
+                        "ochre_title": textid,
+                        "alias": text_info.get("museum_no", text_info["display_name"]),
+                        "description": "Primary publication exemplars:\n{0}".format(
+                            text_info["primary_publication"]
+                        ),
+                    },
+                })
+
+            else: # suhu
+                if "museum_no" not in text_info:
+                    print("no museum_no in ^")
+                my_catalogue.update({
+                    textid: {
+                        "docx_path": docx_path,
+                        "docx_lines": n_lines,
+                        "ochre_title": textid,
+                        "alias": text_info.get("museum_no", text_info["popular_name"]),
+                    },
+                })
+                if "collection" in text_info:
+                    my_catalogue[textid].update({
+                        "description": "Collection:\n{0}".format(
+                            text_info.get("collection", "")
+                        ),
+                    })
 
         # Save to file in docx folders
+        _save_catalogue(my_catalogue, os.path.join(docx_parent_path, folder, "my-catalogue.json"))
 
 
 def main():
